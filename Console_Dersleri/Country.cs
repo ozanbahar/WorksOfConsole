40f673e1cef2abed951c83fa77f20bf58fab8f0f
Ozan Bahar <ozan_bahar@outlook.com>
2024-02-13 00:03:45 +0300

    Add project files.

---
+++ Console_Dersleri/Country.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorksOfConsole
+{
+    internal class Country
+    {
+        //property --> nitelik
+       
+        public int CountryID {  get; set; }
+        public string Name { get; set; }
+        public string Capital { get; set; }
+    }
+}
---
+++ Console_Dersleri/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using WorksOfConsole.Model;
+
+namespace WorksOfConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+
+        }
+
+
+    }
+}
+
